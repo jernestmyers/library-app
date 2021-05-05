9dfd2ac8cca1a1ad112614da69d0130fa930a8e3
jernestmyers <inernestmusic@gmail.com>
2021-05-04 23:27:50 -0500

    added js to instantiate a new book object and then display the info on the table. revised the table structure by specifying thead and tbody. revised the inputs to be wrapped by a form and also changed the 'Add Book' element from a button to an input with the submit attribute. had to add a preventDefault() method to prevent the new book from disappearing immediately after form submittal. need to look into the LocalStorage solution. upon re-reading the instructions, need to look into re-formatting the js so that the addBook function is looping through the myLibrary array. thus, where should i be instantiate the new book object and where should i be pushing it to the array?

--- script.js
+++ script.js
@@ -10,34 +10,58 @@ function Book(title, author, pages, readStatus) {
         // }  FROM THE EXERCISE
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages, readStatus) {
+    const bookToAdd = new Book(title, author, pages, readStatus);
+    const tableDisplay = document.querySelector(`tbody`);
+    const newRow = tableDisplay.insertRow();
+    const newTitleColumn = newRow.insertCell(0);
+    const newAuthorColumn = newRow.insertCell(1);
+    const newPagesColumn = newRow.insertCell(2);
+    const newStatusColumn = newRow.insertCell(3);
+    const removeBookButton = newRow.insertCell(4);
+    newTitleColumn.textContent = title;
+    newAuthorColumn.textContent = author;
+    newPagesColumn.textContent = pages;
+    newStatusColumn.textContent = readStatus;
+    removeBookButton.textContent = `remove?`;
+    myLibrary.push(bookToAdd);
+    console.log(myLibrary);
 }
 
 const userInputFields = document.querySelectorAll(`.userInputs`);
 const inputArray = Array.from(userInputFields);
+const addBookButton = document.querySelector(`#addBook`);
 
-userInputFields.forEach((userInputFields) => {
-    userInputFields.addEventListener(`click`, () => {
-        // const inputArrayValues = Array.from(inputArray.value);
-        // console.log(inputArrayValues);
-        console.log(inputArray[0].value);
-        console.log(inputArray[1].value);
-        console.log(inputArray[2].value);
-        console.log(inputArray[3].value);
-        if (inputArray[0].value !== undefined && inputArray[1].value !== undefined && inputArray[2].value !== undefined && inputArray[3].value !== undefined) {
-            console.log(inputArray[0].value);
-            console.log(inputArray[1].value);
-            console.log(inputArray[2].value);
-            console.log(inputArray[3].value);
-        }
-    })
+addBookButton.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    // console.log(inputArray[0].value);
+    // console.log(inputArray[1].value);
+    // console.log(inputArray[2].value);
+    // console.log(inputArray[3].value);
+    if (inputArray[0].value && inputArray[1].value && inputArray[2].value && inputArray[3].value) {
+        addBookToLibrary(inputArray[0].value, inputArray[1].value, inputArray[2].value, inputArray[3].value)
+    }
 })
+// userInputFields.forEach((userInputFields) => {
+    // userInputFields.addEventListener(`invalid`, (e) => {
+        // const inputArrayValues = Array.from(inputArray.value);
+        // // console.log(inputArrayValues);
+        // console.log(inputArray[0].value);
+        // console.log(inputArray[1].value);
+        // console.log(inputArray[2].value);
+        // console.log(inputArray[3].value);
+        // console.log(userInputFields.checkValidity());
+        // if (inputArray[0].value && inputArray[1].value && inputArray[2].value && inputArray[3].value) {
+            // console.log(e.target.value);
+            // console.log(inputArray[1].value);
+            // console.log(inputArray[2].value);
+            // console.log(inputArray[3].value);
+            // addBookToLibrary(inputArray[0].value, inputArray[1].value, inputArray[2].value, inputArray[3].value)
+        // }
+    // })
+// })
 
 
-// FROM THE EXERCISE
-// const book1 = new Book(`A Sick Day with Amos McGee`, `Don't Know`, `15`, `read`);
-// console.log(book1.bookInfo());
 
 
 
