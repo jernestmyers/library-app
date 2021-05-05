d0a8969639391f10ff883564192b41a6eb009623
jernestmyers <inernestmusic@gmail.com>
2021-05-05 15:01:12 -0500

    Separated the addBook function and the displayBook function. Added an index to pass into the new book objects to use when removing the books, hopefully. Commented out a loop in the displayBook function because it only makes sense to use upon loading an already-populated library display. Beyond that, will need to remove all books before displaying the new library which seems inefficient but possibly necessary once I add removeBook functionality. Last real change was the addition of a Remove button that has a click eventListener attached to it.

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 let myLibrary = [];
 
 function Book(title, author, pages, readStatus) {
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.readStatus = readStatus
-        // this.bookInfo = function() {
-        //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
-        // }  FROM THE EXERCISE
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.readStatus = readStatus,
+        this.libraryIndex = myLibrary.length
 }
 
-function addBookToLibrary(title, author, pages, readStatus) {
+function addBookToLibraryArray(title, author, pages, readStatus) {
     const bookToAdd = new Book(title, author, pages, readStatus);
-    const tableDisplay = document.querySelector(`tbody`);
-    const newRow = tableDisplay.insertRow();
-    const newTitleColumn = newRow.insertCell(0);
-    const newAuthorColumn = newRow.insertCell(1);
-    const newPagesColumn = newRow.insertCell(2);
-    const newStatusColumn = newRow.insertCell(3);
-    const removeBookButton = newRow.insertCell(4);
-    newTitleColumn.textContent = title;
-    newAuthorColumn.textContent = author;
-    newPagesColumn.textContent = pages;
-    newStatusColumn.textContent = readStatus;
-    removeBookButton.textContent = `remove?`;
     myLibrary.push(bookToAdd);
-    console.log(myLibrary);
+    const bookIndex = (myLibrary.length - 1);
+    displayBook(bookIndex, myLibrary);
+}
+
+function displayBook(index, bookArray) {
+    // for (let i = 0; i < bookArray.length; i++) {
+        const tableDisplay = document.querySelector(`tbody`);
+        const newRow = tableDisplay.insertRow();
+        const newTitleColumn = newRow.insertCell(0);
+        const newAuthorColumn = newRow.insertCell(1);
+        const newPagesColumn = newRow.insertCell(2);
+        const newStatusColumn = newRow.insertCell(3);
+        const removeBookColumn = newRow.insertCell(4);
+        const removeBookButton = document.createElement(`button`);
+        newTitleColumn.textContent = bookArray[index].title;
+        newAuthorColumn.textContent = bookArray[index].author;
+        newPagesColumn.textContent = bookArray[index].pages;
+        newStatusColumn.textContent = bookArray[index].readStatus;
+        removeBookColumn.appendChild(removeBookButton);
+        removeBookButton.textContent = `Remove`;
+        removeBookButton.classList.add(`removeBook`);
+        removeBookButton.addEventListener(`click`, removeBookFromLibrary);
+    // }
 }
 
 const userInputFields = document.querySelectorAll(`.userInputs`);
 const inputArray = Array.from(userInputFields);
 const addBookButton = document.querySelector(`#addBook`);
+// const buttonToRemoveBook = document.querySelectorAll(`.removeBook`);
 
 addBookButton.addEventListener(`click`, (e) => {
     e.preventDefault();
@@ -39,9 +48,17 @@ addBookButton.addEventListener(`click`, (e) => {
     // console.log(inputArray[2].value);
     // console.log(inputArray[3].value);
     if (inputArray[0].value && inputArray[1].value && inputArray[2].value && inputArray[3].value) {
-        addBookToLibrary(inputArray[0].value, inputArray[1].value, inputArray[2].value, inputArray[3].value)
+        addBookToLibraryArray(inputArray[0].value, inputArray[1].value, inputArray[2].value, inputArray[3].value)
     }
 })
+
+function removeBookFromLibrary() {
+    console.log(`here`);
+    
+}
+
+
+
 // userInputFields.forEach((userInputFields) => {
     // userInputFields.addEventListener(`invalid`, (e) => {
         // const inputArrayValues = Array.from(inputArray.value);
